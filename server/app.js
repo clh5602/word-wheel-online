4f5b5708e6934556066a671c5e0f52acb4928150
clh5602 <clh5602@rit.edu>
2023-11-29 00:05:49 -0500

    updating the database links

--- server/app.js
+++ server/app.js
@@ -12,12 +12,11 @@ const session = require('express-session');
 const RedisStore = require('connect-redis').default;
 const redis = require('redis');
 
-// const exp = require('constants');
 const router = require('./router.js');
 
 const port = process.env.PORT || process.env.NODE_PORT || 3000;
 
-const dbURI = process.env.MONGODB_URI || 'mongodb://127.0.0.1/DomoMaker';
+const dbURI = process.env.MONGODB_URI || 'mongodb://127.0.0.1/WordWheelOnline';
 mongoose.connect(dbURI).catch((err) => {
   if (err) {
     console.log('Could not connect to database');
